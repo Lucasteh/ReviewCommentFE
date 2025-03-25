29afc6ad7ab6ec40b731a1c2281c1b8704251e25
Lucas Teh <lucast@ilts.com>
2025-03-25 13:13:56 +0800

    testing page animation

--- app/page/signin/page.tsx
+++ app/page/signin/page.tsx
@@ -23,12 +23,11 @@ export default function signin() {
       <div className="grid gap-3 col-start-1 col-end-3 row-start-1">
          <div className="bg-slate-50 p-4 sm:px-8 sm:pt-6 sm:pb-8 lg:p-4 xl:px-8 xl:pt-6 xl:pb-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-4 gap-4 text-sm leading-6">
             <div className="flex">
-               <a href="/new" className="hover:border-blue-500 hover:border-solid hover:bg-white hover:text-blue-500 group w-full flex flex-col items-center justify-center rounded-md border-2 border-dashed border-slate-300 text-sm leading-6 text-slate-900 font-medium py-3">
+               <a href="/new" className="box hover:border-blue-500 hover:border-solid hover:bg-white hover:text-blue-500 group w-full flex flex-col items-center justify-center rounded-md border-2 border-dashed border-slate-300 text-sm leading-6 text-slate-900 font-medium py-3">
                   <svg className="group-hover:text-blue-500 mb-1 text-slate-400" width="20" height="20" fill="currentColor" aria-hidden="true">
                      <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
                   </svg>
-                  <Image src={"/google.png"} width={0} height={0} style={{ width: '100%', height: '100%' }} alt={"TEsting"} />
-                  New project
+                  New project 2312321312321321
                </a>
             </div>
             <div className="flex">
